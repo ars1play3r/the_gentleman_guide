7265c75cde6a24f45042be66b34aae5addc9d478
José Adrian Pupo Montero <154444191+ars1play3r@users.noreply.github.com>
2025-03-26 11:03:28 -0400

    aaaaaa

--- scripts/config.js
+++ scripts/config.js
@@ -5,9 +5,9 @@ import { getAuth } from "firebase/auth";
 // Configuración de Firebase (reemplaza con tus propias credenciales)
 const firebaseConfig = {
   apiKey: "AIzaSyD23dFQX4TaTEPbY4O-IxudPhg8MP-3JOo",
-  authDomain: "mi-proyecto-123.firebaseapp.com",
+  authDomain: "the-gentleman-s-guide-f5354.firebaseapp.com",
   projectId: "the-gentleman-s-guide-f5354",
-  storageBucket: "mi-proyecto-123.appspot.com",
+  storageBucket: "the-gentleman-s-guide-f5354.firebaseapp.com",
   messagingSenderId: "777083189013",
   appId: "1:777083189013:web:589fec4fd356e485169b1c"
 };
