dadc9acbd8c810d9a0cc6de66bbd6a6dc6bfc963
José Adrian Pupo Montero <154444191+ars1play3r@users.noreply.github.com>
2025-03-27 03:57:13 -0400

    Delete src directory

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-// firebaseConfig.js (si divides la configuración)
-import { initializeApp } from 'firebase/app';
-
-const firebaseConfig = {
-  // ... (misma configuración que en config.js)
-};
-
-export const firebaseApp = initializeApp(firebaseConfig);
